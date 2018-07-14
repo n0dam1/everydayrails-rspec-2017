4ed57d7a090745bcb020fbb80ff41b21ccdc7261
n0dam1 <minoru.noda.0527@gmail.com>
2018-07-14 15:36:57 +0900

    Finish section 4

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,35 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    @project = @user.projects.create(
-      name: "Test Project",
-    )
+  # ファクトリで関連するデータを生成する
+  it "genenrates associated data from a factory" do
+    note = FactoryBot.create(:note)
+    puts "This note's project is #{note.project.inspect}"
+    puts "This note's user is #{note.user.inspect}"
   end
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it "is valid with a user, project, and message" do
-    note = Note.new(
-      message: "This is a sample note.",
-      user: @user,
-      project: @project,
-    )
+    note = FactoryBot.build(:note)
     expect(note).to be_valid
   end
 
   # メッセージがなければ無効な状態であること
   it "is invalid without message" do
-    note = Note.new(
-      user: @user,
-      project: @project,
-    )
+    note = FactoryBot.build(:note, message: nil)
     note.valid?
     expect(note.errors[:message]).to include("can't be blank")
   end
@@ -37,18 +24,9 @@ RSpec.describe Note, type: :model do
   # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
     before do
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user,
-      )
-      @note2 = @project.notes.create(
-        message: "This is the second note.",
-        user: @user,
-      )
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven.",
-        user: @user,
-      )
+      @note1 = FactoryBot.create(:note, message: "This is the first note.")
+      @note2 = FactoryBot.create(:note, message: "This is the second note.")
+      @note3 = FactoryBot.create(:note, message: "First, preheat the oven.")
     end
 
     # 一致するデータが見つかるとき
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,20 +3,9 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze"
-    )
-
-    user.projects.create(
-      name: "Test Project"
-    )
-
-    new_project = user.projects.build(
-      name: "Test Project"
-    )
+    owner = FactoryBot.create(:owner)
+    project = FactoryBot.create(:project, name: "Test Project", owner: owner)
+    new_project = FactoryBot.build(:project, name: "Test Project", owner: owner)
 
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
@@ -24,28 +13,39 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    owner = FactoryBot.create(:owner)
+    project = FactoryBot.create(:project, name: "Test Project", owner: owner)
 
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "janetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    other_project = other_user.projects.build(
-      name: "Test Project",
-    )
+    other_owner = FactoryBot.create(:owner, email: "janetester@example.com")
+    other_project = FactoryBot.create(:project, name: "Test Project", owner: other_owner)
 
     expect(other_project).to be_valid
+  end
+
+  # 遅延ステータス
+  describe "late status" do
+    # 締切日が過ぎていれば遅延していること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+
+    # 締切日が今日ならスケジュールどおりであること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+
+    # 締切日が未来ならスケジュールどおりであること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
+  # たくさんのメモが付いていること
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
   end
 end
